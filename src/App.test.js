d0764a074a2c6c9dbcb349ca4fd63a2ad038c169
Jeremy Green <20728342+jg210@users.noreply.github.com>
2018-09-29 17:37:47 +0100

    Add jest unit test that makes sure are using correct node version. I.e. are using nodenv or similar.

--- src/App.test.js
+++ src/App.test.js
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+it('is run with correct node version', () => {
+  expect(process.versions.node).toBe("8.12.0");
+});
+
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
