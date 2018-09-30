5508cd8a343fc349f54d544af1c0e4eb2f600dcb
Jeremy Green <20728342+jg210@users.noreply.github.com>
2018-09-30 17:02:27 +0100

    Set html title.

--- src/index.js
+++ src/index.js
@@ -4,5 +4,6 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+document.title = "FSA Food Hygiene Ratings";
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
