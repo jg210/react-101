0fde8b036a582b42436bce5ce9a37be0149e1887
Jeremy Green <20728342+jg210@users.noreply.github.com>
2019-02-20 21:39:51 +0000

    Comply with FSA API's T&Cs.

--- src/App.js
+++ src/App.js
@@ -19,6 +19,11 @@ class App extends Component {
                 <header className="App-header">
                     <h1 className="App-title">FSA Food Hygiene Ratings</h1>
                 </header>
+                <div>
+                    <p>The information provided here is based on data available via the Food Standards Agency UK Food Hygiene Rating Data API.</p>
+                    <p><a href="https://ratings.food.gov.uk/">https://ratings.food.gov.uk</a></p>
+                    <p><a href="https://www.food.gov.uk/terms-and-conditions">https://www.food.gov.uk/terms-and-conditions</a></p>
+                </div>
                 <Authorities
                     onClick={this.handleLocalAuthorityClick}
                 />
